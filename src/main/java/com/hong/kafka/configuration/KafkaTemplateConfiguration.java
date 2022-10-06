7dfb939db74d6d9fa1844176146da803a3a83aaf
Gidae Hong <70564639+kidae92@users.noreply.github.com>
2022-10-06 18:08:13 +0900

    Update KafkaTemplateConfiguration.java

--- src/main/java/com/hong/kafka/configuration/KafkaTemplateConfiguration.java
+++ src/main/java/com/hong/kafka/configuration/KafkaTemplateConfiguration.java
@@ -26,7 +26,7 @@ public class KafkaTemplateConfiguration {
 
     private Map<String, Object> producerProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "163.180.117.160:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
